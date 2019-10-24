85883e6ec0f62e94fb8723dca7f16c48f9898ac3
Chung Arguello <chung@zipcoders-MacBook-Pro-90.local>
2019-10-24 11:37:16 -0400

    Finished

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -1,4 +1,4 @@
-package rocks.zipcodewilmington.tictactoe;
+ package rocks.zipcodewilmington.tictactoe;
 
 /**
  * @author leon on 6/22/18.
@@ -55,54 +55,63 @@ public class Board {
     }
 
     public Boolean isTie() {
-        if(board[0][0] == 'O' && board[0][1] == 'O'&& board[0][2] == 'O')
-            return false;
-        else if(board[1][0] == 'O' && board[1][1] == 'O'&& board[1][2] == 'O')
-            return false;
-        else if(board[2][0] == 'O' && board[2][1] == 'O'&& board[2][2] == 'O')
-            return false;
-        else if(board[0][0] == 'O' && board[1][0] == 'O'&& board[2][0] == 'O')
-            return false;
-        else if(board[0][1] == 'O' && board[1][1] == 'O'&& board[2][1] == 'O')
-            return false;
-        else if(board[0][2] == 'O' && board[1][2] == 'O'&& board[2][2] == 'O')
-            return false;
-        else if(board[0][0] == 'O' && board[1][1] == 'O'&& board[2][2] == 'O')
-            return false;
-        else if(board[2][0] == 'O' && board[1][1] == 'O'&& board[0][2] == 'O')
-            return false;
-        else
+        ////check rows
+        if(getWinner() == ""){
             return true;
+        }
+
+
+
+        return false;
+//
     }
 
     public String getWinner() {
 
 
-
+            ////check rows
         for(int i = 0; i < 3; i++){
             if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' '){
                 if(board[i][0]== 'X')
-                    return "x";
-                else
+                    return "X";
+                else if(board[i][0] == 'O')
                     return "O";
+                else
+                    return "";
             }
         }
+
+        //check columns
         for(int i = 0; i < 3; i++){
             if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' '){
-                if(board[i][0]== 'X')
-                    return "x";
-                else
+                if(board[0][i]== 'X')
+                    return "X";
+                else if(board[0][i] == 'O')
                     return "O";
-            }
-        }
-        for(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ' ){
-            if(){
+                else
+                    return "";
 
             }
         }
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' '){
+            if(board[0][0]== 'X')
+                return "X";
+            else if(board[0][0] == 'O')
+                return "O";
+            else
+                return "";
+        }
+        if(board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != ' '){
+            if(board[2][0]== 'X')
+                return "X";
+            else if(board[2][0] == 'O')
+                return "O";
+            else
+                return "";
+        }
 
 
-        return " ";
+        return "";
 
     }
 
